be40cfdfbbfec05301edecd0baab2f85773520f3
marknikolop <marknikolop@gmail.com>
2020-04-05 17:53:30 +0200

    submission work

--- src/Success.js
+++ src/Success.js
@@ -1,14 +1,19 @@
 import React, { Component } from 'react';
+import {Link } from "react-router-dom";
 import 'bootstrap/dist/css/bootstrap.min.css';
+import Button from 'react-bootstrap/Button';
 import './resources/Form.css'
 
 
-class Success extends Component{
-    render(){
-        return(
-          
+class Success extends Component {
+    render() {
+        return (
+
             <div>
                 <h1 className="ui centered">Thank you for contributing!</h1>
+                <Link to="/form">
+                    <Button variant="primary">Add your own data here!</Button>
+                </Link>            
             </div>
         )
     }

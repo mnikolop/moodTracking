2682122dbd8800fa2e7c9baf7ff76f404bf25b08
Henrik Wall <henrik@janson.wall.home>
2020-04-05 17:47:45 +0200

    Read kommun.json from database if possible. Otherwise fall back to the file kommun.json Set kommun color based on hits on compination of question and answer hits among responces

--- python/report.py
+++ python/report.py
@@ -1,12 +1,41 @@
 import json
 import numpy as np
 import matplotlib.pyplot as plt
-
 from matplotlib.patches import Circle, Wedge, Polygon
 from matplotlib.collections import PatchCollection
+from collections import Counter
+import requests
 
-with open('kommun.json', 'r') as f:
-    kommuns = json.load(f)
+database_url='https://mytest-f400d.firebaseio.com/'
+
+# Get list of kommuner and kommun data
+try:
+    r = requests.get(database_url + 'kommun.json')
+    kommuns = r.json()
+except ConnectionError:
+    kommuns = None
+
+if kommuns is not None:
+    print "Getting list of kommuns from database"
+
+if kommuns is None:
+    with open('kommun.json', 'r') as f:
+        kommuns = json.load(f)
+
+
+# Get responces from database
+r = requests.get(database_url + 'responces.json')
+responces = r.json()
+
+# Define search criteria
+question='temperature'
+answer='over38'
+
+
+
+locations=[responces[r]['location'] for r in responces if responces[r][question] == answer]
+location_histogram = dict(Counter(locations))
+
 
 xmin = 10e6
 xmax = 0
@@ -15,11 +44,13 @@ ymax = 0
 fig, ax = plt.subplots()
 patches = []
 colors = []
-color = 0
 for k in kommuns['features']:
     name = k['properties']['kommunnamn']
 
-    color = color + 1
+    if name not in location_histogram.keys():
+        color = 0
+    else:
+        color = location_histogram[name]
 
     for a in k['properties']['kommunyta']['coordinates']:
         for area in a:
@@ -41,6 +72,8 @@ ax.add_collection(p)
 ax.set_xlim(xmin,xmax)
 ax.set_ylim(ymin,ymax)
 ax.set_aspect('equal','datalim')
+fig.colorbar(p, ax=ax)
 plt.axis('off')
-plt.savefig("test.png", bbox_inches='tight')
+plt.title('Question "%s" got answer "%s"' % (question, answer))
+plt.savefig("report.png", bbox_inches='tight')
 plt.show()
